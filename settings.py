62e68e1ca248faf56c7c498b6d44fbd947c6a3b6
steak <github@hastake.com>
2011-11-07 16:33:10 +0100

    register courses app

--- settings.py
+++ settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = (
     'splitter',
     'upload',
     'viewers',
+    'courses',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
