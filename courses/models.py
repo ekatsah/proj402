5ef2dea6ad3606ab1731eecc3cb8701821d66eae
steak <github@hastake.com>
2011-11-08 23:26:44 +0100

    finishing the upload view

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from upload.models import Document
 
 class Course(models.Model):
     slug = models.SlugField()
     name = models.TextField()
+    documents = models.ManyToManyField(Document)
+
+    def add_document(self, document):
+        self.documents.add(document)
--- upload/views.py
+++ upload/views.py
@@ -1,14 +1,16 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
-from upload.models import UploadFileForm
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404
+from upload.models import UploadFileForm, Document
 from django.core.urlresolvers import reverse
+from courses.models import Course
 
 def upload_file(request, slug):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            pass
-            # the file is in request.FILES['file']
-
+            course = get_object_or_404(Course, slug=slug)
+            d = Document.new(request.user, course, request.FILES['file'])
+            course.add_document(d)
+            
     # FIXME add an error management
     return HttpResponseRedirect(reverse('course_show', args=[slug]))
