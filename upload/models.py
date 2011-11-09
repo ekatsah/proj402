72d3e85a5ebe5c4fb47edb1e2541df9e902bc50a
steak <github@hastake.com>
2011-11-09 23:03:21 +0100

    add pdf processing capacity

--- upload/models.py
+++ upload/models.py
@@ -1,28 +1,46 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+from settings import UPLOAD_DIR
+from utils.splitter import run_process_file
 
 # Create your models here.
 
 class UploadFileForm(forms.Form):
     file  = forms.FileField()
 
+class Page(models.Model):
+    num = models.IntegerField()
+    filename = models.CharField(max_length=100)
+    width = models.IntegerField()
+    height = models.IntegerField()
+
 class Document(models.Model):
     name = models.TextField()
     owner = models.ForeignKey(User)
     refer = models.ForeignKey('courses.Course', related_name="back_course")
+    size = models.IntegerField(null=True)
+    ready = models.BooleanField(default=False)
+    pages = models.ManyToManyField(Page)
 
     @classmethod
     def new(cls, owner, course, file):
         doc = cls(name=file.name, owner=owner, refer=course)
         doc.save()
-        f = open('upload/r/' + str(doc.pk) + '.pdf', 'w+')
-        f.write(file.read())
-        f.close()
+        run_process_file(doc, file)
         return doc
     
     def get_content(self):
-        f = open('upload/r/' + str(self.pk) + '.pdf', 'r')
+        f = open(UPLOAD_DIR + '/' + str(self.pk) + '.pdf', 'r')
         content = f.read()
         f.close()
         return content
+
+    def set_npages(self, num):
+        self.size = num
+        self.save()
+
+    def add_page(self, num, fname, w, h):
+        p = Page(num=num, filename=fname, width=w, height=h)
+        p.save()
+        self.pages.add(p)
---
+++ utils/splitter.py
@@ -0,0 +1,35 @@
+from settings import UPLOAD_DIR
+from os import system
+from pyPdf import PdfFileReader, PdfFileWriter
+
+def process_file(doc, upfile):
+    filename = UPLOAD_DIR + '/' + str(doc.pk) + '.pdf'
+    # sauvegarde du document original
+    fd = open(filename, 'w')
+    fd.write(upfile.read())
+    fd.close()
+    
+    # sauvegarde du nombre de page
+    fd = file(filename, 'r')
+    pdf = PdfFileReader(fd)
+    doc.set_npages(pdf.numPages)
+    
+    # iteration page a page, transform en png + get page size
+    num = 1
+    for p in pdf.pages:
+        tmp = PdfFileWriter()
+        tmp.addPage(p)
+        out = file("/tmp/%d_cur.pdf" % doc.pk, 'w')
+        tmp.write(out)
+        out.close()
+        
+        pagename = "%s/doc_%03d_%04d.png" % (UPLOAD_DIR, doc.pk, num)
+        system("convert -density 400 /tmp/%d_cur.pdf -resize 25%% %s" % 
+               (doc.pk, pagename))
+        doc.add_page(num, pagename, p.bleedBox.getWidth(), 
+                     p.bleedBox.getHeight())
+        num += 1
+    fd.close()
+
+def run_process_file(doc, file):
+    process_file(doc, file)
