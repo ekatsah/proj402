42f07ba45815786ac4cdfcf8c70f666a5b49c5a3
steak <github@hastake.com>
2012-01-31 19:58:49 +0100

    add some callback for the admin panel

--- admin/views.py
+++ admin/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render_to_response
+from courses.models import Course, Category
+
+def catadd(request, category, subcategory):
+    cat = get_object_or_404(Category, pk=category)
+    subcat = get_object_or_404(Category, pk=subcategory)
+    cat.holds.add(subcat)
+    return HttpResponse('ok', 'text/html')
+
+def catnew(request, category, name):
+    cat = get_object_or_404(Category, pk=category)
+    subcat = Category(name=name, description=".")
+    subcat.save();
+    cat.holds.add(subcat)
+    return HttpResponse('ok', 'text/html')
