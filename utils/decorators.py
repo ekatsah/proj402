66a7b39bc2d2083ae8b925d1c26e49b6777cf5b6
steak <github@hastake.com>
2012-02-01 22:19:25 +0100

    add a decorator to enforce request.POST

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Ajax rewrite.
 def AR(function):
@@ -8,3 +8,12 @@ def AR(function):
 		else:
 			return HttpResponseRedirect('/zoidberg#' + request.path)
 	return check_ajax
+
+# enforce a post request
+def enforce_post(function):
+	def check_post(request, *args, **kwargs):
+		if request.method == 'POST':
+			return function(request, *args, **kwargs)
+		else:
+			return HttpResponse("Error: Not a post request")
+	return check_post
