c9740d6a32ed4828569764fc0dc8f77bc19d3925
steak <github@hastake.com>
2012-02-19 12:07:37 +0100

    increase test buffer

--- utils/authentification.py
+++ utils/authentification.py
@@ -5,6 +5,6 @@ def intra_auth(request):
     sid, uid = request.GET.get("_sid", False), request.GET.get("_uid", False)
     if sid and uid:
         f = urllib2.urlopen('http://www.ulb.ac.be/commons/check?_type=normal&_sid=%s&_uid=%s' % (sid, uid))
-        return HttpResponse("<pre>auth : %s %s\n\n%s</pre>" % (sid, uid, f.read(1000)))
+        return HttpResponse("<pre>auth : %s %s\n\n%s</pre>" % (sid, uid, f.read(10000)))
     else:
         return HttpResponse("auth error #1")
