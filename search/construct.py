cfc085a36ee5d1538b91de8950810c8df801a681
steak <github@hastake.com>
2012-04-11 18:57:17 +0200

    split and keep the words for search

--- search/construct.py
+++ search/construct.py
@@ -1,2 +1,7 @@
+from re import sub
+
 def parse_words(doc, content):
-    print content
+
+    # split and keep the words
+    words = [x.lower() for x in sub(r'\W', ' ', content).split() if len(x) > 2]
+    print str(words) 
