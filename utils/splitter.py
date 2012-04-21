6a063d2d172636e52ccf5eb26d8d0295495ad4e0
steak <github@hastake.com>
2012-04-21 10:20:11 +0200

    refactoring the analyse of uploaded document

--- utils/splitter.py
+++ utils/splitter.py
@@ -1,9 +1,47 @@
-from settings import UPLOAD_DIR
-from django.db import models
+from re import sub
 from os import system
+from settings import UPLOAD_DIR
 from multiprocessing import Process
+from django.db import models, connection
 from pyPdf import PdfFileReader, PdfFileWriter
-from search.construct import parse_words
+
+def parse_words(doc, content):
+    # split and keep the words
+    words = [x.lower() for x in sub(r'\W', ' ', content).split() if len(x) > 2]
+
+    terms_count = dict()
+    for word in words:
+        terms_count[word] = terms_count.get(word, 0) + 1
+
+    cursor = connection.cursor()
+    cursor.execute('select max(id) from search_wordentry;')
+    id = cursor.fetchone()[0];
+    if not id:
+        id = 0
+
+    for word, count in terms_count.iteritems():
+        # too slow : 
+        # WordEntry.objects.create(word=word, document=doc, count=count)
+        id += 1
+        cursor.execute('insert into search_wordentry values(%d, "%s", %d, %d);' %
+                       (id, word, doc.id, count))
+    connection.commit_unless_managed()
+    cursor.close()
+
+    return len(words)
+
+def process_page(doc, page, num, convert):
+    tmp = PdfFileWriter()
+    tmp.addPage(page)
+    out = file("/tmp/%d_cur.pdf" % doc.pk, 'w')
+    tmp.write(out)
+    out.close()
+    pagename = "%s/doc_%03d_%04d.png" % (UPLOAD_DIR, doc.pk, num)
+    if convert:
+        system("convert -density 400 /tmp/%d_cur.pdf -resize 25%% %s" % 
+               (doc.pk, pagename))
+    doc.add_page(num, pagename, page.bleedBox.getWidth(), 
+                 page.bleedBox.getHeight())
 
 def process_file(doc, upfile, convert=True):
     filename = UPLOAD_DIR + '/' + str(doc.pk) + '.pdf'
@@ -25,24 +63,15 @@ def process_file(doc, upfile, convert=True):
 
     # iteration page a page, transform en png + get page size
     num = 1
-    for p in pdf.pages:
-        tmp = PdfFileWriter()
-        tmp.addPage(p)
-        out = file("/tmp/%d_cur.pdf" % doc.pk, 'w')
-        tmp.write(out)
-        out.close()
-        pagename = "%s/doc_%03d_%04d.png" % (UPLOAD_DIR, doc.pk, num)
-        if convert:
-            system("convert -density 400 /tmp/%d_cur.pdf -resize 25%% %s" % 
-                   (doc.pk, pagename))
-        doc.add_page(num, pagename, p.bleedBox.getWidth(), 
-                     p.bleedBox.getHeight())
+    for page in pdf.pages:
+        process_page(doc, page, num, convert)
         num += 1
     fd.close()
 
+# convert used for testing purpose
 def run_process_file(doc, file, convert=True):
     if convert:
-        p = Process(target=process_file, args=(doc, file, True))
+        p = Process(target=process_file, args=(doc, file))
         p.start()
     else:
         process_file(doc, file, False)
