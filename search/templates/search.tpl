6d137dd4963b144adb2647384f9b626f97e14b0b
steak <github@hastake.com>
2012-04-22 20:36:04 +0200

    first tryout of IDF in search

--- search/templates/search.tpl
+++ search/templates/search.tpl
@@ -1,1 +1,23 @@
-{{ msg }}
+<p><strong>This feature is not yet functionnal!</strong></p>
+
+{% if msg %}
+<p>{{ msg }}</p>
+
+{% else %}
+
+{% if rejected %}
+<p><strong>Rejected</strong> : {{ rejected|join:', ' }}</p>
+{% endif %}
+
+<table style="border-collapse:collapse;">
+<tr><th style="width: 100px;">Word</th><th>Present in</th></tr>
+
+{% for word, docs in word_list %}
+<tr><td style="text-align: center; border-top: 1px solid black; border-right: 1px solid black">{{ word }}</td>
+    <td style="border-top: 1px solid black; padding: 8px">{% for id, count, wsize, score in docs %}
+          Doc #{{id}}, score = {{ score }}, {{ count }}/{{ wsize }}<br>
+        {% endfor %}</td></tr>
+{% endfor %}
+</table>
+
+{% endif %}
--- search/views.py
+++ search/views.py
@@ -1,9 +1,23 @@
 from documents.models import Document
 from django.shortcuts import get_object_or_404, render
+from search.models import WordEntry
 
 def search(request):
     if 'q' not in request.GET or request.GET['q'] == '':
         return render(request, 'search.tpl', {'msg': 'You searched nothing!'})
     
-    query = request.GET['q'].split()
-    return render(request, 'search.tpl', {'msg': 'Keywords : ' + str(query)})
+    query, word_list, rejected = request.GET['q'].split(), list(), list()
+    for keyword in query:
+        entries = WordEntry.objects.filter(word=keyword)
+        if len(entries):
+            docs = list()
+            for entry in entries:
+                docs.append((entry.document.id, entry.count, 
+                             entry.document.words, 
+                             entry.document.words/entry.count))
+            word_list.append((keyword, docs))
+        else:
+            rejected.append(keyword)
+
+    return render(request, 'search.tpl', {'rejected': rejected,
+                                          'word_list': word_list})
