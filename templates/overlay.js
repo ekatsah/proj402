8bac5da3df1f1812b19f1bd28fed8b5d07d5bb9c
steak <github@hastake.com>
2012-05-27 02:51:13 +0200

    add missing copyright header

--- templates/overlay.js
+++ templates/overlay.js
@@ -1,3 +1,14 @@
+{% comment %}
+
+# Copyright 2011, hast. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+
+{% endcomment %}
+
 // compute the layout's positions 
 function overlay_refresh() {
 	var l, t;
